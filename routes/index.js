9a635ffd748b980a0d5e97cd6d1c584c3a39c08e
Aleksa Dencic <aleksa.dencic@mozzart.co.yu>
2019-03-19 11:19:17 +0100

    Initial project with Nodemon and EJS template engine installed.

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Dental system Node API' });
+});
+
+module.exports = router;
