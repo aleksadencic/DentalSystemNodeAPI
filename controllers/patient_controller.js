f3a82b42d2ea3a4926fa7a3afde3de90896764c3
Aleksa Dencic <aleksa.dencic@mozzart.co.yu>
2019-03-19 15:46:49 +0100

    Added exceptions in patient files.

--- controllers/patient_controller.js
+++ controllers/patient_controller.js
@@ -7,31 +7,53 @@ exports.find_all_patients = (req, res, next) => {
     app.mysql.query('SELECT * FROM patient', (err, rows, fields) => {
         if (!err) {
             //console.log(rows);
-            res.send(rows);
+            res.status(200).json({
+                type: 'success',
+                count: rows.length,
+                data: rows
+            });
         } else {
-            console.log(err);
+            res.status(500).json({
+                type: 'error',
+                message: 'Internal Server Error',
+                error: err
+            });
         }
     });
 }
 
 // FIND ONE patient
 exports.find_patients_with_id = (req, res, next) => {
-    app.mysql.query('SELECT * FROM patient WHERE patientId = ?', [req.params.id], (err, rows, fields) => {
+    app.mysql.query('SELECT * FROM patient WHERE patientId = ?', [req.params.id], (err, row, fields) => {
         if (!err) {
-            res.send(rows);
+            res.status(200).json({
+                type: 'success',
+                data: row
+            });
         } else {
-            console.log(err);
+            res.status(400).json({
+                type: 'error',
+                message: 'Bad Request. Patient Not Found',
+                error: err
+            });
         }
     });
 }
 
 // DELETE patients
 exports.delete_patient = (req, res, next) => {
-    app.mysql.query('DELETE FROM patient WHERE patientId = ?', [req.params.id], (err, rows, fields) => {
+    app.mysql.query('DELETE FROM patient WHERE patientId = ?', [req.params.id], (err, row, fields) => {
         if (!err) {
-            res.send(rows);
+            res.status(200).json({
+                type: 'success',
+                message: 'Deleted patient successfully'
+            });
         } else {
-            console.log(err);
+            res.status(400).json({
+                type: 'error',
+                message: 'Bad Request. Patient Not Found',
+                error: err
+            });
         }
     });
 }
@@ -47,12 +69,18 @@ exports.insert_patient = (req, res, next) => {
         "Number": req.body.Number,
         "Date_of_birth": req.body.Date_of_birth
     };
-    app.mysql.query('INSERT INTO patient SET  ?', patient, (err, rows, fields) => {
+    app.mysql.query('INSERT INTO patient SET  ?', patient, (err, row, fields) => {
         if (!err) {
-            //console.log(rows);
-            res.send(rows);
+            res.status(200).json({
+                type: 'success',
+                message: 'Inserted patient successfully'
+            });
         } else {
-            console.log(err);
+            res.status(400).json({
+                type: 'error',
+                message: err.message,
+                error: err
+            });
         }
     });
 }
@@ -71,12 +99,18 @@ exports.update_patient = (req, res, next) => {
     app.mysql.query(`UPDATE patient 
                     SET first_name = ?, last_name = ?, JMBG = ?, address = ?, number = ?, date_of_birth = ?
                     WHERE patientID = ?`,
-        [req.body.First_name, req.body.Last_name, req.body.JMBG, req.body.Address, req.body.Number, req.body.Date_of_birth, req.params.id], (err, rows, fields) => {
+        [req.body.First_name, req.body.Last_name, req.body.JMBG, req.body.Address, req.body.Number, req.body.Date_of_birth, req.params.id], (err, row, fields) => {
             if (!err) {
-                //console.log(rows);
-                res.send(rows);
+                res.status(200).json({
+                    type: 'success',
+                    message: 'Updated patient successfully'
+                });
             } else {
-                console.log(err);
+                res.status(400).json({
+                    type: 'error',
+                    message: 'Bad Request. Patient Not Found',
+                    error: err
+                });
             }
         });
 }
