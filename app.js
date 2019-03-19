2aee8a21cead98277074e87c4f9953f98d6ba2f4
Aleksa Dencic <aleksa.dencic@mozzart.co.yu>
2019-03-19 12:34:15 +0100

    Added connection to local MySQL database.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var ejs = require('ejs');
+const mysql = require('mysql');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -24,12 +25,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,6 +38,21 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+var mysql_connect = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'dental_system'
+});
+
+mysql_connect.connect( (err) => {
+  if(!err){
+    console.log('MySQL connection succeded.');
+  } else{
+    console.log('MySQL connection failed. \n Error: ' + JSON.stringify(err, undefined, 2));
+  }
 });
 
 module.exports = app;
