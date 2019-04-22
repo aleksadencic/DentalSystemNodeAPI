e892c75a166df8bea304295627bae6092767391a
Aleksa Dencic <aleksa.dencic@mozzart.co.yu>
2019-04-22 11:54:52 +0200

    Updated CORS for patient

--- routes/patient_routes.js
+++ routes/patient_routes.js
@@ -9,12 +9,12 @@ var patient_controller = require('../controllers/patient_controller');
 
 router.route('/')
     .get(cors(), patient_controller.find_all_patients) // GET request - find all patients
-    .post(patient_controller.insert_patient); // POST request - insert patient
+    .post(cors(), patient_controller.insert_patient); // POST request - insert patient
 
 router.route('/:id')
-    .get(patient_controller.find_patients_with_id) // GET request - find patient with id
-    .put(patient_controller.update_patient) // PUT request - update patient
-    .delete(patient_controller.delete_patient); // DELETE request - delete patient with id
+    .get(cors(), patient_controller.find_patients_with_id) // GET request - find patient with id
+    .put(cors(), patient_controller.update_patient) // PUT request - update patient
+    .delete(cors(), patient_controller.delete_patient); // DELETE request - delete patient with id
 
     
 module.exports = router;
